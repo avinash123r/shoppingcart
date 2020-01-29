4315fb12fad34f1551c1e2601032af5d5dc5f51f
avinash <avinash123r@gmail.com>
2020-01-29 20:13:42 +0530

    Updates

---
+++ precache-manifest.def9649088e688a305a511073f1d2fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04963a223e44bee3bdcde1829e627082",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "c43949320303bdb1b79a",
+    "url": "/shoppingcart/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "39bb9ebb41505da056d7",
+    "url": "/shoppingcart/static/css/main.2f6b6506.chunk.css"
+  },
+  {
+    "revision": "c43949320303bdb1b79a",
+    "url": "/shoppingcart/static/js/2.eb2b11f6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/shoppingcart/static/js/2.eb2b11f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39bb9ebb41505da056d7",
+    "url": "/shoppingcart/static/js/main.b592a623.chunk.js"
+  },
+  {
+    "revision": "b0027766a5c34343037e",
+    "url": "/shoppingcart/static/js/runtime-main.0e3ad91c.js"
+  }
+]);
